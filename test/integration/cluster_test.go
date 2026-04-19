c48d43635c357615e06bfe615200d67b7529b322
Bálits Péter Márton <97095806+balits@users.noreply.github.com>
2026-04-19 09:37:43 +0200

    add(integration-test): status debug on stress writer

--- test/integration/cluster_test.go
+++ test/integration/cluster_test.go
@@ -1465,7 +1465,7 @@ func Test_Integration_Stress_ReadWriteMix(t *testing.T) {
 				key := fmt.Sprintf("mix-%d-%d", writerID, j)
 				status := do(t, l, http.MethodPost, _http.RouteKvPut, api.PutRequest{Key: []byte(key), Value: []byte("val")}, nil)
 				if status != http.StatusOK {
-					writerErrCh <- fmt.Errorf("writer %d failed on key %s", writerID, key)
+					writerErrCh <- fmt.Errorf("writer %d failed on key %s with status", writerID, key, status)
 				}
 			}
 		}(i)
